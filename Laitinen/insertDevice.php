e800cc280045265e3fb9b0a7c190fff8a1d227c8
kilfoofan <arto.vesterbacka@gmail.com>
2018-12-03 18:30:20 +0200

    Add files via upload

--- Laitinen/insertDevice.php
+++ Laitinen/insertDevice.php
@@ -12,6 +12,7 @@ if (isset($_POST['name'])) { // jos lisäysdialogin tallenna-nappia on painettu
     $sn = $_POST['serial'];
     $ir = 0; //isReserved
     $il = 0; //isOnLoan
+    $rs = 0; //reservations
 
     $na = mysqli_real_escape_string($conn, $na);
     $mo = mysqli_real_escape_string($conn, $mo);
@@ -23,14 +24,15 @@ if (isset($_POST['name'])) { // jos lisäysdialogin tallenna-nappia on painettu
     $sn = mysqli_real_escape_string($conn, $sn);
 
     $query = "INSERT INTO ". 
-    "device (NAME, MODEL, MAKE, DESCRIPTION, LOCATION, OWNER, CATEGORY, SERIAL, ISRESERVED, ISONLOAN) ".
-    "VALUES ('".$na."', '".$mo."', '".$ma."', '".$de."', '".$lo."', '".$ow."', '".$ca."', '".$sn."', '".$ir."', '".$il."')";
+    "device (NAME, MODEL, MAKE, DESCRIPTION, LOCATION, OWNER, CATEGORY, SERIAL, ISRESERVED, ISONLOAN, RESERVATIONS) ".
+    "VALUES ('".$na."', '".$mo."', '".$ma."', '".$de."', '".$lo."', '".$ow."', '".$ca."', '".$sn."', '".$ir."', '".$il."', '".$rs."')";
     error_log("insertDevice: ".$query);
     $response = @mysqli_query($conn, $query);
 
     if ($response) { // Jos kysely onnistui, palataan aloitussivulle siten, että parametreina ovat $_SESSION[]-muuttujiin tallennetut hakuehdot
         //header('Location: Teht64.php?name='.$_SESSION['n'].'&address='.$_SESSION['o'].'&asty_avain='.$_SESSION['aa'].'&search=Hae');
         //exit();
+        error_log("insert onnistui)");
     } else {
         echo "<p style='color:red'>Virhe lisättäessä dataa tietokannasta. Viesti: " . mysqli_error($conn) . "</p>";
     }
